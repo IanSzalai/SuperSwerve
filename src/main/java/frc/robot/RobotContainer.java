10458b17e74a40e2f820a55577235184da8fdaaf
ACat701 <90939494+ACat701@users.noreply.github.com>
2022-12-03 14:34:49 -0800

    Added getInRangeOfTag

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,16 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.RobotMap.mapControllers;
 import frc.robot.commands.DriveAbsRotation;
 import frc.robot.commands.DriveSimple;
+import frc.robot.commands.getInRangeOfTag;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Vision;
 
 public class RobotContainer {
 
   private final SN_F310Gamepad conDriver = new SN_F310Gamepad(mapControllers.DRIVER);
   private final Drivetrain subDrivetrain = new Drivetrain();
+  private final Vision subVision = new Vision();
+  private final getInRangeOfTag getInRangeOfTag = new getInRangeOfTag(subVision, subDrivetrain);
 
   public RobotContainer() {
 
@@ -25,7 +29,9 @@ public class RobotContainer {
   private void configureButtonBindings() {
     conDriver.btn_X.whenPressed(new InstantCommand(() -> subDrivetrain.zeroGyroYaw()));
     conDriver.btn_Y.whenPressed(new InstantCommand(() -> subDrivetrain.configure()));
+    conDriver.btn_A.whileHeld(getInRangeOfTag);
   }
+  
 
   public Command getAutonomousCommand() {
     return null;
--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -49,5 +49,14 @@ public class RobotPreferences {
         public static final SN_DoublePreference absSteerOffsetDegrees = new SN_DoublePreference(
                 "absSteerOffsetDegrees", 90);
     }
-
+    
+    public static final class prefVision {
+        // Camera pose relative to robot
+        public static final SN_DoublePreference cameraXPosition = new SN_DoublePreference("cameraXPosition", 0);
+        public static final SN_DoublePreference cameraYPosition = new SN_DoublePreference("cameraYPosition", 0);
+        public static final SN_DoublePreference cameraZPosition = new SN_DoublePreference("cameraZPosition", 0);
+        public static final SN_DoublePreference cameraPitch = new SN_DoublePreference("cameraPitch", 0);
+        public static final SN_DoublePreference cameraYaw = new SN_DoublePreference("cameraYaw", 0);
+        public static final SN_DoublePreference cameraRoll = new SN_DoublePreference("cameraRoll", 0);
+      }
 }
