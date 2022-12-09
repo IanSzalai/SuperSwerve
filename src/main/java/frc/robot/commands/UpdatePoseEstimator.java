51339c0797a878bca64cd21d2528a1da31ee375a
ACat701 <90939494+ACat701@users.noreply.github.com>
2022-12-09 12:17:01 -0800

    Removed unused imports

--- src/main/java/frc/robot/commands/UpdatePoseEstimator.java
+++ src/main/java/frc/robot/commands/UpdatePoseEstimator.java
@@ -7,9 +7,7 @@ package frc.robot.commands;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
-import frc.robot.Constants.constVision.PoseEstimationType;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Vision;
 
